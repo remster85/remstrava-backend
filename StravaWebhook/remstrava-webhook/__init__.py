e877515df80742480782a01febf2d803915b176c
remster85 <remi.desreumaux@gmail.com>
2020-05-01 22:50:30 -0400

    update webhook code to push event to event hub

--- StravaWebhook/remstrava-webhook/__init__.py
+++ StravaWebhook/remstrava-webhook/__init__.py
@@ -1,10 +1,25 @@
 import logging
 import json
 import azure.functions as func
+import requests
+import os
+from azure.eventhub import EventHubProducerClient, EventData
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+        
+    connection_str = os.environ.get("EventHubConnectionString")
+    eventhub_name = 'newactivityhub'
     
+    client  = EventHubProducerClient.from_connection_string(connection_str, eventhub_name=eventhub_name)
+    
+    event_data_batch = client.create_batch()
+    event_data_batch.add(EventData('Message inside EventBatchData'))
+    
+    client.send_batch(event_data_batch)
+    
+
+"""
     hub_challenge  = req.params.get('hub.challenge')
 
     response = {"hub.challenge" : hub_challenge}
@@ -13,3 +28,4 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         json.dumps(response),
         mimetype="application/json",
     )
+"""
